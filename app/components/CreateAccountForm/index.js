6303d0db10c87950bd95e5d3cdb06a8bd7bc5e74
Emmanuel Isaac <deejecks@live.com>
2017-12-24 16:40:14 +0100

    Created CreateAccountForm component

---
+++ app/components/CreateAccountForm/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* CreateAccountForm
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+function CreateAccountForm() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+CreateAccountForm.propTypes = {
+
+};
+
+export default CreateAccountForm;
